a16f713bb02b6ca902dfc327c60accb35bfa63e6
juliapohlmann <julia.pohlmann@case.edu>
2017-01-31 16:10:49 -0500

    set up

---
+++ MyCompanion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyCompanion
+//
+//  Created by Julia Pohlmann on 1/31/17.
+//  Copyright © 2017 EECS395. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

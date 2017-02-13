03d50b9f1fc931949bf5a33dd03e441d6513a5ae
juliapohlmann <julia.pohlmann@case.edu>
2017-02-13 14:18:44 -0500

    lay out done, need to auto constraint

---
+++ MyCompanion/ContactsTableViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ContactsTableViewController.swift
+//  MyCompanion
+//
+//  Created by Julia Pohlmann on 2/13/17.
+//  Copyright © 2017 EECS395. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ContactsTableViewController: UITableViewController {
+    
+}

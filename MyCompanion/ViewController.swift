29ecc2584135d6158aaa2be296c227b9ca86b53f
juliapohlmann <julia.pohlmann@case.edu>
2017-02-15 12:50:01 -0500

    switching for today hello tile working

--- MyCompanion/ViewController.swift
+++ MyCompanion/ViewController.swift
@@ -8,14 +8,70 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
+    @IBOutlet var switchingContainer: UIView!
+    var currentViewController: UIViewController?
+    var controllerIdToSwitchTo: String = "blueComp"
+    var otherControllerId: String = "pinkComp"
     
     override func viewDidLoad() {
+        self.currentViewController = self.storyboard?.instantiateViewController(withIdentifier: "pinkComp")
+        self.currentViewController!.view.translatesAutoresizingMaskIntoConstraints = false
+        self.addChildViewController(self.currentViewController!)
+        self.addSubview(subView: self.currentViewController!.view, toView: self.switchingContainer)
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        startRotation()
 
+    }
+    
+    func startRotation() {
+        var timer = Timer.scheduledTimer(timeInterval: 10.0, target: self, selector: #selector(ViewController.switchView), userInfo: nil, repeats: true)
+        
+    }
+    
+    func switchView() {
+        print("CALLED SWITCH VIEW")
+        let newViewController = self.storyboard?.instantiateViewController(withIdentifier: self.controllerIdToSwitchTo)
+        newViewController!.view.translatesAutoresizingMaskIntoConstraints = false
+        self.cycleFromViewController(oldViewController: self.currentViewController!, toViewController: newViewController!)
+        self.currentViewController = newViewController
+        var temp = self.controllerIdToSwitchTo
+        self.controllerIdToSwitchTo = otherControllerId
+        self.otherControllerId = temp
+    }
+    
+    
+    func cycleFromViewController(oldViewController: UIViewController, toViewController newViewController: UIViewController) {
+        oldViewController.willMove(toParentViewController: nil)
+        self.addChildViewController(newViewController)
+        self.addSubview(subView: newViewController.view, toView:self.switchingContainer!)
+        newViewController.view.alpha = 0
+        newViewController.view.layoutIfNeeded()
+        UIView.animate(withDuration: 0.5, animations: {
+            newViewController.view.alpha = 1
+            oldViewController.view.alpha = 0
+        },
+                                   completion: { finished in
+                                    oldViewController.view.removeFromSuperview()
+                                    oldViewController.removeFromParentViewController()
+                                    newViewController.didMove(toParentViewController: self)
+        })
+    }
+    
+    func addSubview(subView:UIView, toView parentView:UIView) {
+        parentView.addSubview(subView)
+        
+        var viewBindingsDict = [String: AnyObject]()
+        viewBindingsDict["subView"] = subView
+        parentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[subView]|",
+                                                                                 options: [], metrics: nil, views: viewBindingsDict))
+        parentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[subView]|",
+                                                                            options: [], metrics: nil, views: viewBindingsDict))
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.

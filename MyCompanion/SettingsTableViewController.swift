ca257af72fbd002509178bcae2bc539044203a2e
shyamkotak <me@shyamkotak.com>
2017-02-21 21:31:47 -0500

    flesh out caregiver portal, need to add navigation view controller

--- MyCompanion/SettingsTableViewController.swift
+++ MyCompanion/SettingsTableViewController.swift
@@ -11,9 +11,27 @@ import FontAwesome_swift
 
 class SettingsTableViewController: UITableViewController {
 
+    @IBOutlet var storyboardImage: UIImageView!
+    @IBOutlet var contactsImage: UIImageView!
+    @IBOutlet var remindersImage: UIImageView!
+    @IBOutlet var messageImage: UIImageView!
+    @IBOutlet var settingsImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        tableView.tableFooterView = UIView()
+        
+        storyboardImage.image = UIImage.fontAwesomeIcon(name: .book, textColor: UIColor.black, size: CGSize(width: 128, height: 128))
+        
+        contactsImage.image = UIImage.fontAwesomeIcon(name: .user, textColor: UIColor.black, size: CGSize(width: 128, height: 128))
+        
+        remindersImage.image = UIImage.fontAwesomeIcon(name: .list, textColor: UIColor.black, size: CGSize(width: 128, height: 128))
+        
+        messageImage.image = UIImage.fontAwesomeIcon(name: .comment, textColor: UIColor.black, size: CGSize(width: 128, height: 128))
+        
+        settingsImage.image = UIImage.fontAwesomeIcon(name: .cog, textColor: UIColor.black, size: CGSize(width: 128, height: 128))
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -29,13 +47,11 @@ class SettingsTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 5
     }
 
     /*

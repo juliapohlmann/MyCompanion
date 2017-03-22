2d90f454bdc2c60a1d2edd71c6db1d703023f9f6
juliapohlmann <julia.pohlmann@case.edu>
2017-03-22 00:21:31 -0400

    contacts from dashboard working, need to fix photo from data

---
+++ MyCompanion/ContactTableViewCell1.swift
@@ -0,0 +1,31 @@
+//
+//  ContactTableViewCell1.swift
+//  MyCompanion
+//
+//  Created by Julia Pohlmann on 3/22/17.
+//  Copyright © 2017 EECS395. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ContactTableViewCell1: UITableViewCell {
+    
+    @IBOutlet var relationship: UILabel!
+    @IBOutlet var name: UILabel!
+    @IBOutlet var contactImage: UIImageView!
+    @IBOutlet var number: UILabel!
+    @IBOutlet var email: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MyCompanion/ContactTableViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ContactTableViewController.swift
+//  MyCompanion
+//
+//  Created by Julia Pohlmann on 3/21/17.
+//  Copyright © 2017 EECS395. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class ContactTableViewController: UITableViewController {
+    var contacts: [NSManagedObject] = []
+    
+    func dismiss(sender: AnyObject) {
+        
+        self.dismiss(animated: true)
+        
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        fetchContacts()
+        
+        return contacts.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ContactsTableViewCell", for: indexPath) as! ContactTableViewCell1
+        let contact = contacts[indexPath.row]
+        
+        cell.name.text = contact.value(forKeyPath: "name") as? String
+        cell.email.text = contact.value(forKeyPath: "email") as? String
+        cell.number.text = contact.value(forKeyPath: "number") as? String
+        cell.relationship.text = contact.value(forKeyPath: "relationship") as? String
+//        cell.contactImage.image = UIImage(data: contact.value(forKeyPath: "image") as! Data)
+        
+        return cell
+        
+    }
+    
+    func getContext() -> NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
+    }
+    
+    func fetchContacts() {
+        let context = getContext()
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Contact")
+        
+        do {
+            contacts = try context.fetch(fetchRequest)
+        } catch let error as NSError {
+            let errorDialog = UIAlertController(title: "Error!", message: "Failed to save! \(error): \(error.userInfo)", preferredStyle: .alert)
+            errorDialog.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+            present(errorDialog, animated: true)
+        }
+    }
+    
+//    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+//        if editingStyle == .delete {
+//            let context = getContext()
+//            context.delete(contacts[indexPath.row])
+//            
+//            do {
+//                try context.save()
+//            } catch let error as NSError {
+//                let errorDialog = UIAlertController(title: "Error!", message: "Failed to save! \(error): \(error.userInfo)", preferredStyle: .alert)
+//                errorDialog.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+//                present(errorDialog, animated: true)
+//            }
+//            
+//            tableView.deleteRows(at: [indexPath], with: .fade)
+//        }
+//    }
+
+    
+}

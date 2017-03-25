938a59c7924594ceb34796084aba4b5168cfad2e
shyamkotak <me@shyamkotak.com>
2017-03-25 13:42:33 -0400

    settings page

--- MyCompanion/SettingsTableViewController.swift
+++ MyCompanion/SettingsTableViewController.swift
@@ -11,26 +11,11 @@ import FontAwesome_swift
 
 class SettingsTableViewController: UITableViewController {
 
-    @IBOutlet var storyboardImage: UIImageView!
-    @IBOutlet var contactsImage: UIImageView!
-    @IBOutlet var remindersImage: UIImageView!
-    @IBOutlet var messageImage: UIImageView!
-    @IBOutlet var settingsImage: UIImageView!
+    @IBOutlet var callsSwitch: UISwitch!
+    @IBOutlet var emailsSwitch: UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        tableView.tableFooterView = UIView()
-        
-        storyboardImage.image = UIImage.fontAwesomeIcon(name: .book, textColor: UIColor.black, size: CGSize(width: 128, height: 128))
-        
-        contactsImage.image = UIImage.fontAwesomeIcon(name: .user, textColor: UIColor.black, size: CGSize(width: 128, height: 128))
-        
-        remindersImage.image = UIImage.fontAwesomeIcon(name: .list, textColor: UIColor.black, size: CGSize(width: 128, height: 128))
-        
-        messageImage.image = UIImage.fontAwesomeIcon(name: .comment, textColor: UIColor.black, size: CGSize(width: 128, height: 128))
-        
-        settingsImage.image = UIImage.fontAwesomeIcon(name: .cog, textColor: UIColor.black, size: CGSize(width: 128, height: 128))
         
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -44,16 +29,17 @@ class SettingsTableViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+    override func tableView(_ tableView: UITableView,
+                            titleForHeaderInSection section: Int) -> String? {
+        
+        switch(section){
+            
+        case 0: return "Contacts Settings"
+        default: return "mistake"
+            
+        }
+        
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
-    }
-
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)

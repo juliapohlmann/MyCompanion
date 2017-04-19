18a4a6395156c23a6fd0366d35c9a650a795afe3
shyamkotak <me@shyamkotak.com>
2017-04-19 17:20:04 -0400

    caregiver tips

--- MyCompanion/CaregiverTipsTableViewController.swift
+++ MyCompanion/CaregiverTipsTableViewController.swift
@@ -10,114 +10,104 @@ import UIKit
 
 class CaregiverTipsTableViewController: UITableViewController {
     
-    var tips:[Any] = []
+    var tips:[NSDictionary] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.tableFooterView = UIView(frame: .zero)
         
-        print("here")
-        
         do {
             if let file = Bundle.main.url(forResource: "tips", withExtension: "json") {
                 let data = try Data(contentsOf: file)
                 let json = try JSONSerialization.jsonObject(with: data, options: [])
-                if let object = json as? [String: Any] {
-                    // json is a dictionary
-                    print(object)
-                } else if let object = json as? [Any] {
+                if let object = json as? [Any] {
                     // json is an array
-                    tips = object["SubCat"]
+                    tips = object as! [NSDictionary]
                     self.tableView.reloadData()
                 } else {
                     print("JSON is invalid")
                 }
             } else {
                 print("no file")
-            }        } catch {
+            }
+        } catch {
             print(error.localizedDescription)
         }
-        
-        print(tips)
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-    }
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.tips.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+        let currentCellInfo = tips[convertIndexPathToRow(indexPath: indexPath)] as NSDictionary
+        
         let cell:UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "caregiverTipsCell")!
-        cell.textLabel?.text = self.tips[indexPath.row] as? String
+        cell.textLabel?.text = currentCellInfo["SubCat"] as? String
         return cell
         
     }
     
-    // MARK: - Table view data source
-    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
+        
+        return 5
+        
     }
     
-    /*
-     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-     let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-     
-     // Configure the cell...
-     
-     return cell
-     }
-     */
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        switch(section) {
+        case 0:
+            return 4
+        case 1:
+            return 5
+        case 2:
+            return 13
+        case 3:
+            return 3
+        case 4:
+            return 4
+        default: return 0
+        }
+
+    }
     
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
+    override func tableView(_ tableView: UITableView,
+                            titleForHeaderInSection section: Int) -> String? {
+        
+        switch(section){
+            
+        case 0: return "Well-Being"
+        case 1: return "Communication"
+        case 2: return "Behaviors"
+        case 3: return "Daily Care"
+        default: return "Activities"
+            
+        }
+        
+    }
     
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
+    func convertIndexPathToRow(indexPath: IndexPath) -> Int {
+        
+        switch(indexPath.section){
+            
+        case 0: return indexPath.row
+        case 1: return indexPath.row + 4
+        case 2: return indexPath.row + 9
+        case 3: return indexPath.row + 22
+        case 4: return indexPath.row + 25
+        default: return indexPath.row
+            
+        }
+        
+    }
     
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let nextScene = segue.destination as? DisplayTipsViewController
+        let indexPath = self.tableView.indexPathForSelectedRow
+        let currentCellInfo = tips[convertIndexPathToRow(indexPath: indexPath!)] as NSDictionary
+        
+        nextScene?.text = (currentCellInfo["Info"] as? String)!
+
+    }
     
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
     
 }

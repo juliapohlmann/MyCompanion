dcd5b1157c28668e1e1ff095ae52326b5dcf1997
juliapohlmann <julia.pohlmann@case.edu>
2017-04-27 20:52:35 -0400

    added memory book page that is shown when no pages have been created

--- MyCompanion/ViewMemoryBookViewController.swift
+++ MyCompanion/ViewMemoryBookViewController.swift
@@ -13,6 +13,7 @@ class ViewMemoryBookViewController: UIViewController {
 
     var pages: [NSManagedObject] = []
     var pageNum = 0
+    
     @IBOutlet var previousButton: UIButton!
     @IBOutlet var nextButton: UIButton!
     @IBOutlet var containerView: UIView!
@@ -24,51 +25,38 @@ class ViewMemoryBookViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //UIColor(red: 247.00, green: 247.00, blue: 147.00, alpha: 1.00)
         nextIcon.image = UIImage.fontAwesomeIcon(name: .arrowRight, textColor: UIColor.black, size: CGSize(width: 93, height: 81))
         
         prevIcon.image = UIImage.fontAwesomeIcon(name: .arrowLeft, textColor: UIColor.black, size: CGSize(width: 93, height: 81))
 
-        
         pages = fetchPages()
+        
         
         if(pages.count != 0) {
             updateView()
         } else {
+            toggleAllPrevious(isHidden: true)
+            toggleAllNext(isHidden: true)
             
-            self.previousButton.isHidden = true
-            self.nextButton.isHidden = true
-            
+            let controller = self.storyboard!.instantiateViewController(withIdentifier: "LabelPageViewController") as! LabelPageViewController
+            controller.pageText = "Go to Caregiver Portal to add pages to your memory book! You can add text, videos, and photos to create a custom memory book that tells the story of your life."
+            controller.pageTitle = "Create your memory book now!"
+            finishSetup(controller: controller)
         }
         
     }
     
     func updateView() {
         
-        self.previousButton.isHidden = false
-        self.nextButton.isHidden = false
-        self.prevIcon.isHidden = false
-        self.nextIcon.isHidden = false
-        self.prevLabel.isHidden = false
-        self.nextLabel.isHidden = false
-        
-        //self.willMove(toParentViewController: nil)
-        //self.collectionView.removeFromSuperview()
-        //self.removeFromParentViewController()
+        toggleAllPrevious(isHidden: false)
+        toggleAllNext(isHidden: false)
         
         if(pageNum == 0) {
-            
-            self.previousButton.isHidden = true
-            self.prevIcon.isHidden = true
-            self.prevLabel.isHidden = true
-            
+            toggleAllPrevious(isHidden: true)
         }
+        
         if(pageNum + 1 == pages.count) {
-            
-            self.nextButton.isHidden = true
-            self.nextIcon.isHidden = true
-            self.nextLabel.isHidden = true
-            
+            toggleAllNext(isHidden: true)
         }
         
         let pageType = pages[pageNum].value(forKeyPath: "templateType") as? String
@@ -103,12 +91,20 @@ class ViewMemoryBookViewController: UIViewController {
                 controller.videoID = pages[pageNum].value(forKeyPath: "videoID") as! String
             }
             
-            
             finishSetup(controller: controller)
         }
-        
-        
-        
+    }
+    
+    func toggleAllPrevious(isHidden: Bool) {
+        self.previousButton.isHidden = isHidden
+        self.prevIcon.isHidden = isHidden
+        self.prevLabel.isHidden = isHidden
+    }
+    
+    func toggleAllNext(isHidden: Bool) {
+        self.nextButton.isHidden = isHidden
+        self.nextIcon.isHidden = isHidden
+        self.nextLabel.isHidden = isHidden
     }
     
     func finishSetup(controller: UIViewController) {
